6a28c67f10da8c11cc9686df35579d62369b9871
Amarildo Lacerda <amarildo_51@msn.com>
2015-03-07 11:48:24 -0300

    querySample

---
+++ samples/query/queryBFiredac.dpr
@@ -0,0 +1,14 @@
+program queryBFiredac;
+
+uses
+  Vcl.Forms,
+  wBdeFiredac in 'wBdeFiredac.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
